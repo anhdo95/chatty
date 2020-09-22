b224e674e9582aeb8619cd08b857d41430c6e7e9
Anh Do <dinhanhdo95@gmail.com>
2020-09-22 22:57:25 +0700

    Add dependencies

---
+++ core/axios.ts
@@ -0,0 +1,29 @@
+import axios from "axios"
+import Cookies from 'js-cookie'
+
+const instance = axios.create({
+    baseURL: process.env.BASE_URL,
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+instance.interceptors.request.use(config => {
+  if (Cookies.get('token')) {
+    config.params = config.params || {}
+    config.params.auth = Cookies.get('token')
+  }
+  
+  return config
+})
+
+instance.interceptors.response.use(res => {
+  if (res) {
+    return res.data
+  }
+
+  return res
+})
+
+export default instance
